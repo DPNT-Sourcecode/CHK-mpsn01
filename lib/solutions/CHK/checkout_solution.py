9915d944d37035e071f9f6d1cb02bb5ebe8648f4
sbx_user1051 <sbx_user1051@169.254.89.101>
2024-09-19 20:47:51 +0000

    Thu Sep 19 20:47:51 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,8 +3,17 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus) -> int:
+    valid = ['A', 'B', 'C', 'D']
+    
     # check for no items added
     if skus == "":
         return 0
     
+    # loop through SKUS, return -1 if any characters not valid
+    for char in skus:
+        # check char is valid
+        if char not in valid:
+            return -1 
+    
     return -1
+
