34a1c1ca20cd7a35c754373ee64d86575acee745
sbx_user1051 <sbx_user1051@169.254.89.101>
2024-09-19 20:41:51 +0000

    Thu Sep 19 20:41:51 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,9 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus) -> int:
+    # check for no items added
+    if skus == "":
+        return 0
+    
+    return -1
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,6 +1,8 @@
 from solutions.CHK import checkout_solution
 
 
-class TestSum():
-    def test_sum(self):
-        pass
+class TestCheckout():
+    def test_checkout_no_input(self):
+        # Test case 1 - empty string
+        result = checkout_solution.checkout("")
+        assert isinstance (result, int)
