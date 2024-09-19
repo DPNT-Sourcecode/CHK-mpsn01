e7de56fee939beb01e36407f5af87649138d66f0
sbx_user1051 <sbx_user1051@169.254.68.253>
2024-09-19 21:12:25 +0000

    Thu Sep 19 21:12:25 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,6 +11,7 @@ def checkout(skus) -> int:
         return 0
     
     # TODO: Initialise a class to represent the checkout
+    checkout = Checkout()
     
     # loop through SKUS, return -1 if any characters not valid
     for char in skus:
@@ -19,3 +20,26 @@ def checkout(skus) -> int:
             return -1 
     
     return result
+
+# Class is used to represent the checkout
+class Checkout:
+    def __init__(self):
+        pass
+    def add_item(self):
+        pass
+    def remove_item(self):
+        pass
+    def total(self):
+        pass
+
+# Class is used to represent an item, how much it costs
+# and how many have been selected
+class Item:
+    def __init__(self):
+        pass
+    def add_one(self):
+        pass
+    def remove_one(self):
+        pass
+    def total(self):
+        pass
