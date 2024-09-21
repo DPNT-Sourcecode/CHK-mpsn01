33d36f6b1bbb863e178511f07541bbfde0a1777c
sbx_user1051 <sbx_user1051@169.254.121.205>
2024-09-21 16:57:08 +0000

    Sat Sep 21 16:57:08 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -121,9 +121,9 @@ class TestCheckoutClass():
         assert checkout.receipt == {}
         assert checkout.items == {
             'A':50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G':20, 'H':10,
-            'I':35, 'J':60, 'K':80, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 
-            'Q':30, 'R':50, 'S':30, 'T':20, 'U':40, 'V':50, 'W':20, 'X':90,
-            'Y':10, 'Z': 50}
+            'I':35, 'J':60, 'K':70, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 
+            'Q':30, 'R':50, 'S':20, 'T':20, 'U':40, 'V':50, 'W':20, 'X':17,
+            'Y':20, 'Z':21}
         
     def test_add_item(self):
         checkout = checkout_solution.Checkout()
@@ -392,7 +392,7 @@ class TestItemClass():
         item = checkout_solution.Item('K', 80)
         item.add_one()
         item.add_one()
-        assert item.apply_single_discount_offer() == 150
+        assert item.apply_single_discount_offer() == 120
         
         # Test case 3 - 3 x K
         item = checkout_solution.Item('K', 80)
