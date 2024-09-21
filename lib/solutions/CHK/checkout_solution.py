457634fe693d10711df9941596d2514f655ef72a
sbx_user1051 <sbx_user1051@169.254.29.29>
2024-09-21 15:44:27 +0000

    Sat Sep 21 15:44:27 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -58,8 +58,10 @@ class Checkout:
             
     def get_multi_item_discount(self) -> int:
         # define eligible items and discounts
+        # example: '[[2,15], [{'E':2}]]' index 1 represents the number of E required to get a
+        # free B, index 0 represents the discount available if 2B are purchased.
         discount = 0
-        offers = {'B': [[2,15], [{'E':2}]], 'M':{'N':3}, 'Q': [[3,80], {'R':3}]}
+        offers = {'B': [[2,15], [{'E':2}]], 'M':{'N':3}, 'Q': [[3,10], {'R':3}]}
         
         if len(self.receipt) == 0:
             return 0
@@ -187,6 +189,7 @@ class Item:
         
         total = self.count*self.price
         return total
+
 
 
 
