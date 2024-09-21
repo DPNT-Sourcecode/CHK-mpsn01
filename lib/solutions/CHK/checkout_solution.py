55893b064ebda4678b520543a566864ae649c390
sbx_user1051 <sbx_user1051@169.254.121.205>
2024-09-21 16:54:08 +0000

    Sat Sep 21 16:54:08 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -36,7 +36,7 @@ class Checkout:
     def __init__(self):
         self.items = {
             'A':50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G':20, 'H':10,
-            'I':35, 'J':60, 'K':80, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 
+            'I':35, 'J':60, 'K':70, 'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 
             'Q':30, 'R':50, 'S':30, 'T':20, 'U':40, 'V':50, 'W':20, 'X':90,
             'Y': 10,'Z': 50}
         self.receipt = {}
@@ -131,7 +131,7 @@ class Item:
             
     # used to apply discounts in the format 2B for 45
     def apply_single_discount_offer(self):
-        items = {'K':[2,150],'P': [5,200]}
+        items = {'K':[2,120],'P': [5,200]}
         
         total = 0
         num_items = self.count
